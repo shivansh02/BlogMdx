b0253f621065f2964f25e1b4aa766f8623ce64f7
shivansh02 <ss398@snu.edu.in>
2024-07-02 04:45:18 +0530

    desc updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Next Dev Blog",
+  title: "BlogMdx",
   description:
-    "A dev blog with Next.js app router, TailwindCSS, and contentlayer",
+    "Easily publish dev blog posts from Markdown files."
 };
 
 interface RootLayoutProps {
